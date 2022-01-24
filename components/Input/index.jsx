ee30870d7991c176149c6c19d465fde30aae62ee
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2022-01-24 22:35:00 +0530

    Added input  component for the app

--- components/Input/index.jsx
+++ components/Input/index.jsx
@@ -1,28 +1,102 @@
-import React from "react";
-import { Text, TextInput, View, Dimensions } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  Dimensions,
+  StyleSheet,
+  Animated,
+  Easing,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-function Input() {
-  const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
+
+function Input(props) {
+  const [isFocused, setIsFocused] = useState(false);
+  const focusAnim = useRef(new Animated.Value(0)).current;
+  const inputRef = useRef(null);
+
+  const { style, onBlur, onFocus, value, label, errorText, ...restOfProps } =
+    props;
+
+  let color = isFocused ? "#080F9C" : "#B9C4CA";
+  if (errorText) {
+    color = "#B00020";
+  }
+
+  useEffect(() => {
+    Animated.timing(focusAnim, {
+      toValue: isFocused || !!value ? 1 : 0,
+      duration: 150,
+      easing: Easing.bezier(0.4, 0, 0.2, 1),
+      useNativeDriver: false,
+    }).start();
+  }, [isFocused, focusAnim, value]);
+
   return (
-    <View
-      style={{
-        width: 0.8 * width,
-        marginHorizontal: 0.1 * width,
-      }}
-    >
-      <Text style={{ fontSize: 16, fontWeight: "bold" }}>Email</Text>
-      <View style={{ marginTop: 8 }}>
-        <TextInput
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            borderRadius: 8,
-            height: 0.07 * height,
-          }}
-        />
-      </View>
+    <View style={style}>
+      <TextInput
+        onBlur={(event) => {
+          setIsFocused(false);
+          onBlur?.(event);
+        }}
+        onFocus={(event) => {
+          setIsFocused(true);
+          onFocus?.(event);
+        }}
+        style={[styles.input, style]}
+        {...restOfProps}
+      />
+      <TouchableWithoutFeedback onPress={() => inputRef.current?.focus()}>
+        <Animated.View
+          style={[
+            styles.labelContainer,
+            {
+              top: focusAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [24, -6],
+              }),
+            },
+          ]}
+        >
+          <Animated.Text
+            style={{
+              fontSize: focusAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [16, 12],
+              }),
+              color,
+            }}
+          >
+            {label}
+            {errorText ? "*" : ""}
+          </Animated.Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      {!!errorText && <Text style={styles.error}>{errorText}</Text>}
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 24,
+    borderWidth: 1,
+    borderRadius: 4,
+    fontSize: 16,
+  },
+  labelContainer: {
+    position: "absolute",
+    left: 16,
+    paddingHorizontal: 8,
+    backgroundColor: "white",
+  },
+  error: {
+    marginTop: 4,
+    marginLeft: 12,
+    color: "#B00020",
+  },
+});
--- screens/LoginScreen/index.jsx
+++ screens/LoginScreen/index.jsx
@@ -1,17 +1,37 @@
 import React from "react";
-import { SafeAreaView, Platform, View, StatusBar } from "react-native";
+import {
+  SafeAreaView,
+  Platform,
+  View,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 import Input from "../../components/Input";
 function LoginScreen() {
+  const { width, height } = Dimensions.get("window");
+
   return (
     <SafeAreaView
       style={{
-        backgroundColor: "#f2f2f2",
+        backgroundColor: "#ffffff",
         flex: 1,
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
       }}
     >
       <View style={{ flex: 1, backgroundColor: "#f2f2f2" }}>
-        <Input />
+        <View
+          style={{
+            marginHorizontal: width * 0.05,
+            width: width * 0.9,
+            marginTop: 24,
+          }}
+        >
+          <Input
+            placeholder="Enter email"
+            style={{ backgroundColor: "white" }}
+            label="Email"
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
